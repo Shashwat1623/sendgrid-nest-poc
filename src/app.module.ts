4ee7e19ecbabb77e4902bbc26ae9de4479c8fe1a
Shashwat <sdixit@argusoft.com>
2026-01-09 15:23:47 +0530

    Minor change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InboundEmailController } from './inbound-email/inbound-email.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [InboundEmailController],
-  providers: [],
+  controllers: [InboundEmailController,AppController],
+  providers: [AppService],
 })
 export class AppModule {}
